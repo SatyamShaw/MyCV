83b761f4870588645023904a5e0ed7168ba0bbb1
Satyam  Kumar Shaw <55422880+SatyamShaw@users.noreply.github.com>
2021-12-07 16:45:10 +0530

    Add files via upload

---
+++ scripts/diceGame_index.js
@@ -0,0 +1,16 @@
+var randomNumber1= Math.ceil(Math.random()*6);
+var randomNumber2= Math.ceil(Math.random()*6);
+
+document.querySelector(".img1").setAttribute("src", "images/dice"+ randomNumber1+".png");
+document.querySelector(".img2").setAttribute("src", "images/dice"+ randomNumber2+".png");
+
+document.querySelector("h1").setAttribute("src", "images/dice"+ randomNumber2+".png");
+
+if(randomNumber1> randomNumber2)
+  document.querySelector("h1").innerText="Player1 Won, Refresh to play again"
+ else if(randomNumber1 == randomNumber2) {
+   document.querySelector("h1").innerText="Draw!, Refresh to play again"
+ }
+ else  {
+   document.querySelector("h1").innerText="Player2 Won, Refresh to play again"
+ }
