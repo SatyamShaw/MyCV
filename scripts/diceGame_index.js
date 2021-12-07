2a8f7632f977c877d50473aba273400435004f9e
Satyam  Kumar Shaw <55422880+SatyamShaw@users.noreply.github.com>
2021-12-07 20:55:39 +0530

    Update diceGame_index.js

--- scripts/diceGame_index.js
+++ scripts/diceGame_index.js
@@ -1,10 +1,8 @@
 var randomNumber1= Math.ceil(Math.random()*6);
 var randomNumber2= Math.ceil(Math.random()*6);
 
-document.querySelector(".img1").setAttribute("src", "images/dice"+ randomNumber1+".png");
-document.querySelector(".img2").setAttribute("src", "images/dice"+ randomNumber2+".png");
-
-document.querySelector("h1").setAttribute("src", "images/dice"+ randomNumber2+".png");
+document.querySelector(".img1").setAttribute("src", "Images/dice"+ randomNumber1+".png");
+document.querySelector(".img2").setAttribute("src", "Images/dice"+ randomNumber2+".png");
 
 if(randomNumber1> randomNumber2)
   document.querySelector("h1").innerText="Player1 Won, Refresh to play again"
