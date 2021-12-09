fc7315c6e98d7e17f8356e199d02061efe9ddccf
Satyam  Kumar Shaw <55422880+SatyamShaw@users.noreply.github.com>
2021-12-09 19:04:00 +0530

    Add files via upload

--- scripts/diceGame_index.js
+++ scripts/diceGame_index.js
@@ -1,16 +1,22 @@
-var randomNumber1= Math.ceil(Math.random()*6);
-var randomNumber2= Math.ceil(Math.random()*6);
 
-document.querySelector(".img1").setAttribute("src", "images/dice"+ randomNumber1+".png");
-document.querySelector(".img2").setAttribute("src", "images/dice"+ randomNumber2+".png");
+document.querySelector(".rollDice").addEventListener("click", rollDice);
 
-document.querySelector("h1").setAttribute("src", "images/dice"+ randomNumber2+".png");
+function rollDice()
+{
+  var randomNumber1= Math.ceil(Math.random()*6);
+  var randomNumber2= Math.ceil(Math.random()*6);
 
-if(randomNumber1> randomNumber2)
-  document.querySelector("h1").innerText="Player1 Won, Refresh to play again"
- else if(randomNumber1 == randomNumber2) {
-   document.querySelector("h1").innerText="Draw!, Refresh to play again"
- }
- else  {
-   document.querySelector("h1").innerText="Player2 Won, Refresh to play again"
- }
+  document.querySelector(".img1").setAttribute("src", "images/dice"+ randomNumber1+".png");
+  document.querySelector(".img2").setAttribute("src", "images/dice"+ randomNumber2+".png");
+
+  document.querySelector("h1").setAttribute("src", "images/dice"+ randomNumber2+".png");
+
+  if(randomNumber1> randomNumber2)
+    document.querySelector("h1").innerText="Player1 Won"
+   else if(randomNumber1 == randomNumber2) {
+     document.querySelector("h1").innerText="Draw!"
+   }
+   else  {
+     document.querySelector("h1").innerText="Player2 Won"
+   }
+}
